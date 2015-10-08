f3032c01f041b7c4e3261b9d6e284fc2ca8fda65
EduardoVaca <vacalalo@gmail.com>
2015-10-07 20:14:05 -0500

    Login Successfull

---
+++ Thinner/app/src/main/java/com/example/eduardovaca/thinner/ParseApplication.java
@@ -0,0 +1,32 @@
+package com.example.eduardovaca.thinner;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+/**
+ * Created by eduardovaca on 07/10/15.
+ */
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        
+        Parse.initialize(this, "MpfbhzgP7ceDtcCCWjlc2tlZ3d9qA7EeGg5y0PkG", "UseJCvMg8g63CP6yRk5Z013Uu1T9L7VnMzmrQ2XO");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+
+}
