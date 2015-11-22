323cdce114ed71fbd081034f14103521cdd1507d
EduardoVaca <vacalalo@gmail.com>
2015-11-22 13:29:27 -0600

    Updated following diet

--- Thinner/app/src/main/java/com/example/eduardovaca/thinner/MyDietsActivity.java
+++ Thinner/app/src/main/java/com/example/eduardovaca/thinner/MyDietsActivity.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.parse.FindCallback;
@@ -11,6 +13,7 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -47,5 +50,44 @@ public class MyDietsActivity extends AppCompatActivity {
             }
         });
 
+        myDietsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                changeFollowingDiet(position);
+            }
+        });
+
+    }
+
+
+    private void changeFollowingDiet(int index){
+
+        loading = new ProgressDialog(MyDietsActivity.this, AlertDialog.THEME_HOLO_LIGHT);
+        loading.setMessage("Cargando");
+        loading.show();
+
+        ParseUser.getCurrentUser().put("actualDiet", objects[index].getObjectId());
+        ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                loading.hide();
+
+                if(e == null){
+                    ParseQuery<ParseObject> query = new ParseQuery<>(DIET_CLASS);
+                    query.whereEqualTo("userId", ParseUser.getCurrentUser());
+                    query.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> list, ParseException e) {
+                            loading.hide();
+                            if (e == null) {
+                                objects = list.toArray(new ParseObject[list.size()]);
+                                DietListAdapter dietAdapter = new DietListAdapter(MyDietsActivity.this, R.layout.item_my_diets, objects);
+                                myDietsList.setAdapter(dietAdapter);
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
