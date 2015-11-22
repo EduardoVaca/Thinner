7c14b6fa6a07f0e13f265400a12e1b70c986c997
EduardoVaca <vacalalo@gmail.com>
2015-11-22 12:08:51 -0600

    Created layout for list of diets

---
+++ Thinner/app/src/main/java/com/example/eduardovaca/thinner/MyDietsActivity.java
@@ -0,0 +1,13 @@
+package com.example.eduardovaca.thinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyDietsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_diets);
+    }
+}
