8fcc52f1f474703f198dd5d9b903d3434b34a928
EduardoVaca <vacalalo@gmail.com>
2015-11-22 11:11:58 -0600

    Added spinners

--- Thinner/app/src/main/java/com/example/eduardovaca/thinner/InsertMealActivity.java
+++ Thinner/app/src/main/java/com/example/eduardovaca/thinner/InsertMealActivity.java
@@ -7,14 +7,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.SaveCallback;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InsertMealActivity extends AppCompatActivity {
 
@@ -26,6 +33,11 @@ public class InsertMealActivity extends AppCompatActivity {
     private Button addDish;
     private Button finish;
     private TextView listTV;
+    private Spinner timeSpinner;
+    private Spinner daySpinner;
+
+    private static final String daysWeek [] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private static final String timeDay [] = {"Dawn", "Morning", "Noon", "Afternoon", "Evening"};
 
     String list;
 
@@ -37,12 +49,24 @@ public class InsertMealActivity extends AppCompatActivity {
         name = (EditText) findViewById(R.id.name_food_et);
         units= (EditText) findViewById(R.id.units_food_et);
         quantity = (EditText) findViewById(R.id.quantity_food_et);
-        time = (EditText) findViewById(R.id.time_food_et);
-        day = (EditText) findViewById(R.id.day_food_et);
         addDish = (Button) findViewById(R.id.add_dish_btn);
         finish = (Button) findViewById(R.id.finish_btn);
         listTV  = (TextView) findViewById(R.id.list_dishes);
+        timeSpinner = (Spinner) findViewById(R.id.time_spinner);
+        daySpinner = (Spinner) findViewById(R.id.day_spinner);
         list = "";
+
+        List<String> dayList = new ArrayList<>(Arrays.asList(daysWeek));
+        ArrayAdapter<String> dayAdapter = new ArrayAdapter<String>(InsertMealActivity.this,
+                                                    android.R.layout.simple_spinner_item, dayList);
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(dayAdapter);
+
+        List<String> timeList = new ArrayList<>(Arrays.asList(timeDay));
+        ArrayAdapter<String> timeAdapter = new ArrayAdapter<String>(InsertMealActivity.this,
+                                                    android.R.layout.simple_spinner_item, timeList);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
 
         addDish.setOnClickListener(new View.OnClickListener() {
             @Override
